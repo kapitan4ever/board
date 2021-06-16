4980940f78bbbfa9cdeaa22902a2cef6858e011e
kapitan4ever <80713212+kapitan4ever@users.noreply.github.com>
2021-06-16 11:58:27 +0300

    добавлен способ вывода, результат тот же

--- app.js
+++ app.js
@@ -6,25 +6,47 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseover', setColor)
+  //2 способ
+  //square.addEventListener('mouseover', () => setColor(square))
 
-  square.addEventListener('mouseleave', () => removeColor(square))
+  square.addEventListener('mouseleave', removeColor)
+  //2 способ
+  //square.addEventListener('mouseleave', () => removeColor(square))
 
   board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target
   const color = getRandomColor()
   element.style.backgroundColor = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+//2 способ
+/*function setColor(element) {
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}*/
+
+function removeColor(event) {
+  const element = event.target
   element.style.backgroundColor = '#1d1d1d'
   element.style.boxShadow = `0 0 2px #000`
 }
+//2 способ
+/*function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d'
+  element.style.boxShadow = `0 0 2px #000`
+}*/
 
 function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+// или
+/* function getRandomColor() {
   const index = Math.floor(Math.random() * colors.length)
   return colors[index]
-}
+} */
